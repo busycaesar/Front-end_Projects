b429e3f03836cf1b0374e4812d97fe2aa8067594
Dev Shah <busycaesar@gmail.com>
2022-08-08 19:50:43 -0400

    A5: Instructions implimented.

--- Taste_Of_Vadodara/src/js/app.js
+++ Taste_Of_Vadodara/src/js/app.js
@@ -18,135 +18,95 @@
 // Create local variables to work with it in this file.
 const { products, categories } = window;
 
-window.onload = () => 
-{
-
+window.onload = () => {
   // VARIABLE DECLARATION.
   let x = document.getElementById("menu");
   let i = 0;
-  
-  categories.forEach(() => 
-  {
-
+  categories.forEach(() => {
     // VARIABLE DECLARATION
     let temp = categories[i].name;
     let button = document.createElement("button");
-    
     button.setAttribute("id", "btn" + i);
     button.setAttribute("onclick", "clk(id);");
     button.innerHTML = temp;
     x.appendChild(button);
     i++;
-  
   });
-
 };
 
-function clk(id) 
-{
-
+function clk(id) {
   // VARIABLE DECLARATION
   let button = document.getElementById(id),
     name,
     prod,
     title = document.getElementById("selected-category"),
     table = document.getElementById("category-products");
-  
-    if (button.innerHTML == categories[0].name) 
-    {
-
+  if (button.innerHTML == categories[0].name) {
     name = categories[0].name;
     table.innerHTML = "";
-
     prod = products.filter(
       (element) => element.categories == categories[0].id && element.discontinued == false
     );
-
-    prod.forEach(function (element) 
-    {
-
-      console.log(element);
-
-      /*
-      // VARIABLE DECLARATION.
-      let proTable = document.createElement("tr");
-      let proTitle = document.createElement("td");
-      let proDisc = document.createElement("td");
-      let proPrice = document.createElement("td");
-      
-      proTable.setAttribute("onclick", "console.log(categories[0].name);");
-      
-      proTitle.innerHTML = element.title;
-      proDisc.innerHTML = element.description;
-      proPrice.innerHTML = new Intl.NumberFormat("de-DE", {
-        style: "currency",
-        currency: "CAD"
-      }).format(element.price);
-      
-      proTable.appendChild(proTitle);
-      proTable.appendChild(proDisc);
-      proTable.appendChild(proPrice);
-      table.appendChild(proTable);
-
-      */
-    
+    prod.forEach(function (element) {
+      createCard(element);
     });
-  }
-   else if (button.innerHTML == categories[1].name) 
-   {
-
+  } else if (button.innerHTML == categories[1].name) {
     name = categories[1].name;
     table.innerHTML = "";
     prod = products.filter(
       (element) => element.categories == categories[1].id && element.discontinued == false
     );
     prod.forEach(function (element) {
-      // VARIABLE DECLARATION.
-      let proTable = document.createElement("tr");
-      let proTitle = document.createElement("td");
-      let proDisc = document.createElement("td");
-      let proPrice = document.createElement("td");
-      proTable.setAttribute("onclick", "console.log(categories[1].name);");
-      proTitle.innerHTML = element.title;
-      proDisc.innerHTML = element.description;
-      proPrice.innerHTML = new Intl.NumberFormat("de-DE", {
-        style: "currency",
-        currency: "CAD"
-      }).format(element.price);
-      proTable.appendChild(proTitle);
-      proTable.appendChild(proDisc);
-      proTable.appendChild(proPrice);
-      table.appendChild(proTable);
+      createCard(element);
     });
-  }
-   else 
-   {
-
+  } else {
     name = categories[2].name;
     table.innerHTML = "";
     prod = products.filter(
       (element) => element.categories == categories[2].id && element.discontinued == false
     );
     prod.forEach(function (element) {
-      // VARIABLE DECLARATION.
-      let proTable = document.createElement("tr");
-      let proTitle = document.createElement("td");
-      let proDisc = document.createElement("td");
-      let proPrice = document.createElement("td");
-      proTable.setAttribute("onclick", "console.log(categories[2].name);");
-      proTitle.innerHTML = element.title;
-      proDisc.innerHTML = element.description;
-      proPrice.innerHTML = new Intl.NumberFormat("de-DE", {
-        style: "currency",
-        currency: "CAD"
-      }).format(element.price);
-      proTable.appendChild(proTitle);
-      proTable.appendChild(proDisc);
-      proTable.appendChild(proPrice);
-      table.appendChild(proTable);
+      createCard(element);
     });
   }
   title.innerText = name;
+}
+
+function createCard(element) {
+  // VARIABLE DECLARATION.
+  let cardDiv = document.createElement("div");
+  let innerDiv = document.createElement("div");
+  let frontDiv = document.createElement("div");
+  let imageOfProduct = document.createElement("img");
+  let backDiv = document.createElement("div");
+  let title = document.createElement("h4");
+  let description = document.createElement("p");
+  let price = document.createElement("p");
+  let mainDiv = document.getElementById("category-products");
+
+  cardDiv.setAttribute("class", "flip-card");
+  innerDiv.setAttribute("class", "flip-card-inner");
+  frontDiv.setAttribute("class", "flip-card-front");
+  backDiv.setAttribute("class", "flip-card-back");
+  imageOfProduct.setAttribute("src", element.image);
+  imageOfProduct.setAttribute("alt", element.title);
+  imageOfProduct.setAttribute("style", "width:300px;height:300px;");
+  title.innerHTML = element.title;
+  description.innerHTML = element.description;
+  price.innerHTML = new Intl.NumberFormat("de-DE", {
+    style: "currency",
+    currency: "CAD"
+  }).format(element.price);
+
+  frontDiv.appendChild(imageOfProduct);
+  backDiv.appendChild(title);
+  backDiv.appendChild(description);
+  backDiv.appendChild(price);
+  innerDiv.appendChild(frontDiv);
+  innerDiv.appendChild(backDiv);
+  cardDiv.appendChild(innerDiv);
+  mainDiv.appendChild(cardDiv);
+  console.log(mainDiv);
 }
 
 // For debugging, display all of our data in the console

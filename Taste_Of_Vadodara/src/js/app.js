6d8762600d2d5b739c8505476a0e37fb78d8f55a
Dev Shah <busycaesar@gmail.com>
2022-08-08 18:23:48 -0400

    A5: Initial Commit.

--- Taste_Of_Vadodara/src/js/app.js
+++ Taste_Of_Vadodara/src/js/app.js
@@ -18,54 +18,83 @@
 // Create local variables to work with it in this file.
 const { products, categories } = window;
 
-window.onload = () => {
+window.onload = () => 
+{
+
   // VARIABLE DECLARATION.
   let x = document.getElementById("menu");
   let i = 0;
-  categories.forEach(() => {
+  
+  categories.forEach(() => 
+  {
+
     // VARIABLE DECLARATION
     let temp = categories[i].name;
     let button = document.createElement("button");
+    
     button.setAttribute("id", "btn" + i);
     button.setAttribute("onclick", "clk(id);");
     button.innerHTML = temp;
     x.appendChild(button);
     i++;
+  
   });
+
 };
 
-function clk(id) {
+function clk(id) 
+{
+
   // VARIABLE DECLARATION
   let button = document.getElementById(id),
     name,
     prod,
     title = document.getElementById("selected-category"),
     table = document.getElementById("category-products");
-  if (button.innerHTML == categories[0].name) {
+  
+    if (button.innerHTML == categories[0].name) 
+    {
+
     name = categories[0].name;
     table.innerHTML = "";
+
     prod = products.filter(
       (element) => element.categories == categories[0].id && element.discontinued == false
     );
-    prod.forEach(function (element) {
+
+    prod.forEach(function (element) 
+    {
+
+      console.log(element);
+
+      /*
       // VARIABLE DECLARATION.
       let proTable = document.createElement("tr");
       let proTitle = document.createElement("td");
       let proDisc = document.createElement("td");
       let proPrice = document.createElement("td");
+      
       proTable.setAttribute("onclick", "console.log(categories[0].name);");
+      
       proTitle.innerHTML = element.title;
       proDisc.innerHTML = element.description;
       proPrice.innerHTML = new Intl.NumberFormat("de-DE", {
         style: "currency",
         currency: "CAD"
       }).format(element.price);
+      
       proTable.appendChild(proTitle);
       proTable.appendChild(proDisc);
       proTable.appendChild(proPrice);
       table.appendChild(proTable);
+
+      */
+    
     });
-  } else if (button.innerHTML == categories[1].name) {
+  }
+   else if (button.innerHTML == categories[1].name) 
+   {
+
     name = categories[1].name;
     table.innerHTML = "";
     prod = products.filter(
@@ -89,7 +118,10 @@ function clk(id) {
       proTable.appendChild(proPrice);
       table.appendChild(proTable);
     });
-  } else {
+  }
+   else 
+   {
+
     name = categories[2].name;
     table.innerHTML = "";
     prod = products.filter(
